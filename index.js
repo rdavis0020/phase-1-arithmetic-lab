b22245394826b594dc732417c484f5bd1be73684
Ryan Davis <rdavis0020@gmail.com>
2023-08-19 00:27:48 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+const num3 = 20;
+const num4 = 8;
+
+const mod = num3 % num4;
+const numbers = [10, 5, 20, 15, 8];
+const max = Math.max(...numbers);
